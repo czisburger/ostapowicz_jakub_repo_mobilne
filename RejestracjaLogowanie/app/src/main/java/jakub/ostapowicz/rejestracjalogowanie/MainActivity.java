6808c0a901fdf3c1d8d856a7929a614c3b62c22f
unknown <dreon20k@gmail.com>
2022-01-05 13:25:22 +0100

    0.8

---
+++ RejestracjaLogowanie/app/src/main/java/jakub/ostapowicz/rejestracjalogowanie/MainActivity.java
@@ -0,0 +1,14 @@
+package jakub.ostapowicz.rejestracjalogowanie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jo_aplikacja1);
+    }
+}
